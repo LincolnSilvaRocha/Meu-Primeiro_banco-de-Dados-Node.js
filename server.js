490d58c68e808d33a5bb987993864a9cf19292ed
linksaint <lincolnrocha1987@gmail.com>
2025-02-26 16:29:31 -0300

    Banco-de-dados-node.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import express from 'express'
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 const app = express()
+
 app.use(express.json())
+app.use(cors())
+
+const users = []
 
 
-app.get('/usuarios/:abacate', async (req, res) => {
+app.get('/usuarios', async (req, res) => {
     
     const users = await prisma.user.findMany()
     
